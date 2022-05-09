721328844417f173a671ae562ba77708cdb51274
HarishKamaras <harishkamarasu@gmail.com>
2022-05-09 16:17:31 +0530

    DeckOfCards

---
+++ src/com/brigdelabz/objectorientedprograms/DeckOfCards.java
@@ -0,0 +1,7 @@
+package com.brigdelabz.objectorientedprograms;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("Welcome to DeckOfCards");
+    }
+}
